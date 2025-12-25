660ee543de1a1b3611c06878b4dabc3940badb6a
etranger2268 <shun.5.2268@gmail.com>
2025-12-25 16:37:27 +0900

    develop-03-18

--- src/lessons/Lesson2/Lesson2_1.tsx
+++ src/lessons/Lesson2/Lesson2_1.tsx
@@ -1,7 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+type Position = {
+  x: number;
+  y: number;
+};
 
 export default function Lesson2_1() {
-  useEffect(() => {}, []);
+  const initialPosition: Position = { x: 0, y: 0 };
+  const [position, setPosition] = useState(initialPosition);
+
+  const handleMove = (e: PointerEvent) => setPosition({ x: e.clientX, y: e.clientY });
+
+  // biome-ignore lint: "handleMove is not included in the dependency"
+  useEffect(() => {
+    window.addEventListener('pointermove', handleMove);
+  }, []);
 
   return (
     <div
@@ -11,6 +24,7 @@ export default function Lesson2_1() {
         borderRadius: '50%',
         opacity: 0.6,
         pointerEvents: 'none',
+        transform: `translate(${position.x}px, ${position.y}px)`,
         left: -20,
         top: -20,
         width: 50,
