f9d92ff91c3b1e2be2cc27d15ca4829b7bc833f0
etranger2268 <shun.5.2268@gmail.com>
2025-12-25 15:54:43 +0900

    develop-02-10

--- src/lessons/Lesson1/Lesson1_1.tsx
+++ src/lessons/Lesson1/Lesson1_1.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { type ChangeEvent, useState } from 'react';
 
 export default function Lesson1_1() {
   const [age, setAge] = useState<number>(0);
+  const [value, setValue] = useState<string>('apple');
 
   console.log('rendering');
 
@@ -9,16 +10,22 @@ export default function Lesson1_1() {
     setAge((prev) => prev + 1);
     setAge((prev) => prev + 1);
     setAge((prev) => prev + 1);
-
   };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => setValue(e.target.value);
 
   return (
     <div>
-      <input type="text" />
+      <input
+        type="text"
+        value={value}
+        onChange={handleChange}
+        className="border mr-2 p-2 rounded focus:outline-none focus:ring focus:ring-blue-500"
+      />
       <button type="button" onClick={handleClick} className="border p-2 rounded-md bg-red-100">
         Add age
       </button>
-      <p>You are {age}</p>
+      <p className="mt-2">You are {age}</p>
     </div>
   );
 }
