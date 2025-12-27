055d20120448c098be11e82b3f734102d1fa945e
etranger2268 <shun.5.2268@gmail.com>
2025-12-27 19:35:03 +0900

    develop-05-32

--- src/lessons/Lesson4/context/Auth.tsx
+++ src/lessons/Lesson4/context/Auth.tsx
@@ -1,5 +1,10 @@
 import { createContext, useContext, useState } from 'react';
 
+type UserInfo = {
+  username: string;
+  email: string;
+};
+
 const AuthContext = createContext(false);
 
 export const useAuth = () => {
@@ -7,9 +12,23 @@ export const useAuth = () => {
   return context;
 };
 
-const AuthProvider = () => {
-  const [user, setUser] = useState(null);
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<UserInfo | null>(null);
 
-  const login = () => {};
-  const logout = () => {};
+  const login = (userInfo: UserInfo) => {
+    if (userInfo.username === 'textUser' && userInfo.email === 'test@gmail.com') {
+      setUser(userInfo);
+    } else {
+      console.log(`Can't logged in`);
+    }
+  };
+  const logout = () => setUser(null);
+
+  const contextValue = {
+    user,
+    login,
+    logout,
+  };
+
+  return <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>;
 };
