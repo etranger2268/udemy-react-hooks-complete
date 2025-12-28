39fa750b57f942e5547799d9b8543acb0bf5cb7c
etranger2268 <shun.5.2268@gmail.com>
2025-12-28 14:12:08 +0900

    develop-07-47

--- src/lessons/Lesson6/Thread.tsx
+++ src/lessons/Lesson6/Thread.tsx
@@ -1,19 +1,43 @@
-export default function Thread({ messages, sendMessage }) {
-  const handleSubmit = async () => {};
+import { useRef } from 'react';
+import type { Message } from './Lesson6';
+
+type Props = {
+  messages: Message[];
+  sendMessage: (formData: FormData) => Promise<void>;
+};
+
+export default function Thread({ messages, sendMessage }: Props) {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!formRef.current) return;
+    const formData = new FormData(formRef.current);
+    await sendMessage(formData);
+    formRef.current?.reset();
+  };
+
   return (
-    <form onSubmit={handleSubmit} className="flex items-center gap-2">
-      <input
-        type="text"
-        name="message"
-        placeholder="Hello!"
-        className="border py-0.5 px-2 rounded outline-none focus:ring focus:ring-blue-600"
-      />
-      <button
-        type="submit"
-        className="bg-sky-400 py-0.5 px-2 text-white rounded transition-all duration-300 hover:bg-sky-600"
-      >
-        送信
-      </button>
-    </form>
+    <div>
+      <ul className="flex items-center">
+        {messages.map((message) => (
+          <li key={message.key}>{`${message.text},`}</li>
+        ))}
+      </ul>
+      <form ref={formRef} onSubmit={handleSubmit} className="flex items-center gap-2">
+        <input
+          type="text"
+          name="message"
+          placeholder="Hello!"
+          className="border py-0.5 px-2 rounded outline-none focus:ring focus:ring-blue-600"
+        />
+        <button
+          type="submit"
+          className="bg-sky-400 py-0.5 px-2 text-white rounded transition-all duration-300 hover:bg-sky-600"
+        >
+          送信
+        </button>
+      </form>
+    </div>
   );
 }
