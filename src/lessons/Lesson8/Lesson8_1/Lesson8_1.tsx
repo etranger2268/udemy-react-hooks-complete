e60bf36cfae9ed6e43cdd45b0dc0c2bbc66fc802
etranger2268 <shun.5.2268@gmail.com>
2025-12-29 14:32:44 +0900

    develop-09-61

--- src/lessons/Lesson8/Lesson8_1/Lesson8_1.tsx
+++ src/lessons/Lesson8/Lesson8_1/Lesson8_1.tsx
@@ -1,8 +1,9 @@
-import { Suspense, useState } from 'react';
+import { Suspense, useDeferredValue, useState } from 'react';
 import SearchResult from './SearchResult';
 
 export default function Lesson8_1() {
   const [query, setQuery] = useState<string>('');
+  const deferredQuery = useDeferredValue(query);
   return (
     <div className="text-lg">
       <label htmlFor="search" className="block">
@@ -13,10 +14,10 @@ export default function Lesson8_1() {
         placeholder="album's title"
         value={query}
         onChange={(e) => setQuery(e.currentTarget.value)}
-        className="border py-0.5 px-2 rounded outline-none focus:ring focus:ring-blue-600"
+        className={`border py-0.5 px-2 rounded outline-none focus:ring focus:ring-blue-600 ${query !== deferredQuery ? 'opacity-20' : 'opacity-100'}`}
       />
       <Suspense fallback={<p className="text-green-600">Loading...</p>}>
-        <SearchResult query={query} />
+        <SearchResult query={deferredQuery} />
       </Suspense>
     </div>
   );
