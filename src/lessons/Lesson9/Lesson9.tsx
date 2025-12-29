b685525ccea495613f01e769555f6de2162d5700
etranger2268 <shun.5.2268@gmail.com>
2025-12-29 15:22:22 +0900

    develop-10-70

--- src/lessons/Lesson9/Lesson9.tsx
+++ src/lessons/Lesson9/Lesson9.tsx
@@ -1,16 +1,28 @@
-import { formAction } from './actions/actions';
+import { useActionState } from 'react';
+import { action } from './actions/actions';
+
+export type State = {
+  count: number;
+};
 
 export default function Lesson9() {
+  const initialState: State = { count: 0 };
+  const [state, formAction, isPending] = useActionState(action, initialState);
   return (
     <div className="text-lg">
       <form action={formAction}>
         <button
+          value={`Current Button Count: ${state.count}`}
+          name="button"
           type="submit"
-          className="bg-sky-400 text-white py-0.5 px-4 rounded transition-all duration-300 hover:bg-sky-600"
+          disabled={isPending}
+          className={`text-white py-0.5 px-4 rounded transition-all duration-300 ${isPending ? 'bg-gray-400' : 'bg-sky-400 hover:bg-sky-600'}`}
         >
-          Increment
+          {isPending ? 'Updating...' : 'Increment'}
         </button>
       </form>
+
+      {state && <p>Count: {state.count}</p>}
     </div>
   );
 }
--- src/lessons/Lesson9/actions/actions.ts
+++ src/lessons/Lesson9/actions/actions.ts
@@ -1,5 +1,11 @@
 'use server';
 
-export const formAction = async () => {
-  console.log('server action');
+import type { State } from '../Lesson9';
+
+export const action = async (prevState: State, formData: FormData) => {
+  console.log(`Button Value: ${formData.get('button')}`);
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  return {
+    count: (prevState?.count || 0) + 1,
+  };
 };
